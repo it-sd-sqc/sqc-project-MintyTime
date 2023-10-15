eda9bc827e2361729f3eb30d935ff21d5adc4b60
Bhayes9 <Bradleyhayes1999@gmail.com>
2023-10-15 12:12:25 -0500

    ran npm generate successfully

--- docs/generated-schema.sql
+++ docs/generated-schema.sql
@@ -1,1 +1,34 @@
+DROP TABLE IF EXISTS chapter CASCADE;
+ DROP TABLE IF EXISTS pageNumber;
+
+CREATE TABLE chapter (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL
+);
+
+CREATE TABLE pageNumber (
+  id SERIAL PRIMARY KEY,
+  page_id INT NOT NULL, 
+  title TEXT NOT NULL,
+  number INT NOT NULL,
+  FOREIGN KEY (page_id) REFERENCES chapter(id)
+);
+
+
+INSERT INTO chapter (title) VALUES
+('' ),
+('CHAP. II.' ),
+('CHAP. III.' ),
+('CHAP. IV.' ),
+('CHAP. V.' ),
+('CHAP. VI.' ),
+('CHAP. VII.' ),
+('CHAP. VIII.' ),
+('CHAP. IX.' ),
+('CHAP. X.' ),
+('CHAP. XI.' ),
+('CHAP. XII.' ),
+('CHAP. XIII.' ),
+('CONCLUDING CHAPTER.' ),
+('' );
 
