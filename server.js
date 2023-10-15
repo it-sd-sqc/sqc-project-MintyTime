9523c9130065053484522dc730d19901b35b19a4
Bhayes9 <Bradleyhayes1999@gmail.com>
2023-10-15 13:30:48 -0500

    added routes

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ const PORT = process.env.PORT || 5163
 // Web server setup ////////////////////////////////////////
 const app = express()
 //app.use(express.static('./public'))
-  app.use(express.static('./public'))
+  app.use(express.static('public'))
   app.set('views', 'pages')
   app.set('view engine', 'ejs')
   
+  // Routes //////////////////////////////////////////////////
+  .get('/', function (req, res) {
+    res.render('pages/index')
+  })
+  .get('/about', function (req, res) {
+    res.render('pages/about', { title: 'About' })
+  })
+ 
 // Ready for browsers to connect ///////////////////////////
 const displayPort = function () {
   console.log('Listening on ' + PORT)
