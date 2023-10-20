e23bf8e42f6420f537c92d89600ad3ec38ea972e
Bhayes9 <Bradleyhayes1999@gmail.com>
2023-10-19 21:32:18 -0500

    changed css and refined webpages

--- public/page.js
+++ public/page.js
@@ -1,14 +1,20 @@
 document.querySelector('button').addEventListener('click', function () {
   const body = document.querySelector('body')
   const main = document.querySelector('main')
+  const footer = document.querySelector('footer')
+  const header = document.querySelector('header')
   const button = document.querySelector('button')
   if (body.getAttribute('data-theme') === 'dark') {
     body.removeAttribute('data-theme')
     main.removeAttribute('data-theme')
+    footer.removeAttribute('data-theme')
+    header.removeAttribute('data-theme')
     button.textContent = '🌙'
   } else {
     body.setAttribute('data-theme', 'dark')
     main.setAttribute('data-theme', 'dark')
+    footer.setAttribute('data-theme', 'dark')
+    header.setAttribute('data-theme', 'dark')
     button.textContent = '☀️'
   }
 })
